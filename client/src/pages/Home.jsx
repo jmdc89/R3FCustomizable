d9125e0438db258df596fc9daec8465bed221299
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-10 08:40:24 +0300

    Update Home.jsx
    motion header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,9 +12,21 @@ import {
 
 const Home = () => {
   const snap = useSnapshot(state);
-  
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src='./threejs.png'
+              alt="logo"
+              className='w-8 h-8 object-contain'
+            />
+          </motion.header>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
