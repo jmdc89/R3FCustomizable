abde1fb9988fa6fbf1a1050ff64e200fabafc0fb
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-10 08:46:42 +0300

    Update Home.jsx
    home content div

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,14 +16,25 @@ const Home = () => {
   return (
     <AnimatePresence>
       {snap.intro && (
-        <motion.section className='home' {...slideAnimation('left')}>
+        <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
             <img
               src='./threejs.png'
               alt="logo"
-              className='w-8 h-8 object-contain'
+              className="w-8 h-8 object-contain"
             />
           </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+
+            
+
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
