1cdb2200846ebfd01a87f353f9bf6734efaad616
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-14 16:29:06 +0300

    Update Shirt.jsx
    constants

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -7,8 +7,22 @@ import { Decal, useGLTF, useTexture} from '@react-three/drei';
 import state from '../store';
 
 const Shirt = () => {
+    const snap = useSnapshot(state);
+    const {nodes, materials} = useGLTF('/shirt_baked.glb')
+
+    const logoTexture = useTexture(snap.logoDecal);
+    const fullTexture = useTexture(snap.fullDecal);
+
   return (
-    <div>Shirt</div>
+    <group>
+        <mesh
+            castShadow
+            geometry={}
+            //minute 55
+        >
+
+        </mesh>
+    </group>
   )
 }
 
