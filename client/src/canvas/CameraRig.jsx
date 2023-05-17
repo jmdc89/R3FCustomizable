d8f405fdbd0cfd64c8aeb8ca9621007444297567
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-17 16:26:22 +0300

    Update CameraRig.jsx
    rotation model

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -10,6 +10,14 @@ const CameraRig = ({children}) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
+   // set the model rotation smoothly
+   easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x / 5, 0],
+    0.25,
+    delta
+  )
+
   return <group ref={group}>{children}</group>
 }
 
