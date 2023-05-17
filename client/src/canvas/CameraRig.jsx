65743062da52aa98dbad22e1087dd38bd4b13291
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-17 16:05:41 +0300

    Update CameraRig.jsx
    imports

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
 
 const CameraRig = () => {
   return (
