1c196375540ec9e723023bf323cbff77474ee711
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-05-18 14:31:37 +0300

    Update CameraRig.jsx
    useFrame delta

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -6,19 +6,21 @@ import { useSnapshot } from 'valtio';
 import state from '../store';
 
 
-const CameraRig = ({children}) => {
+const CameraRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
-   // set the model rotation smoothly
-   easing.dampE(
-    group.current.rotation,
-    [state.pointer.y / 10, -state.pointer.x / 5, 0],
-    0.25,
-    delta
-  )
+  useFrame((state, delta) => {
 
-  //min59
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+
 
   return <group ref={group}>{children}</group>
 }
